84383b8c992650e1657cdddccdaf376cebe68bad
lulublake <praiseonyeguke@gmail.com>
2023-06-25 17:04:15 +0100

    Initial commit

---
+++ tenaryassignment.js
@@ -0,0 +1,9 @@
+//Question 1
+let age = 18;
+
+age >= 18 ? console.log("Eligible to vote") : console.log("Too young to vote"); 
+
+//Question 2
+let number = 5;
+
+number % 2 == 0? console.log("The number is an even number") : console.log("The number is an odd number");
